ad9902549d6a1a239b6071afa72b097a5eec80e4
Thomas Logan <thomaslilogan@gmail.com>
2016-12-30 13:07:13 -0800

    switch to memcpy

--- int32x4.c
+++ int32x4.c
@@ -1,73 +1,60 @@
+#include <string.h>
+
+
 typedef int v4si __attribute__ ((vector_size (16), aligned(16)));
 
 void add (unsigned char * pa, unsigned char * pb, unsigned char * pc) {
   v4si a, b, c;
-  a = *((v4si *) pa);
-  //memcpy(&a, pa, 16);
-  b = *((v4si *) pb);
-  //memcpy(&b, pb, 16);
+  memcpy(&a, pa, 16);
+  memcpy(&b, pb, 16);
   c = a + b;
-  *((v4si *) pc) = c;
-  //memcpy(pc, &c, 16);
-  return;
+  memcpy(pc, &c, 16);
 }
 
 void mult (unsigned char * pa, unsigned char * pb, unsigned char * pc) {
   v4si a, b, c;
-  a = *((v4si *) pa);
-  b = *((v4si *) pb);
+  memcpy(&a, pa, 16);
+  memcpy(&b, pb, 16);
   c = a * b;
-  *((v4si *) pc) = c;
-  //memcpy(pc, &c, 16);
-  return;
+  memcpy(pc, &c, 16);
 }
 
 void subt (unsigned char * pa, unsigned char * pb, unsigned char * pc) {
   v4si a, b, c;
-  a = *((v4si *) pa);
-  b = *((v4si *) pb);
+  memcpy(&a, pa, 16);
+  memcpy(&b, pb, 16);
   c = a - b;
-  *((v4si *) pc) = c;
-  //memcpy(pc, &c, 16);
-  return;
+  memcpy(pc, &c, 16);
 }
 
 void divi (unsigned char * pa, unsigned char * pb, unsigned char * pc) {
   v4si a, b, c;
-  a = *((v4si *) pa);
-  b = *((v4si *) pb);
+  memcpy(&a, pa, 16);
+  memcpy(&b, pb, 16);
   c = a / b;
-  *((v4si *) pc) = c;
-  //memcpy(pc, &c, 16);
-  return;
+  memcpy(pc, &c, 16);
 }
 
 void modu (unsigned char * pa, unsigned char * pb, unsigned char * pc) {
   v4si a, b, c;
-  a = *((v4si *) pa);
-  b = *((v4si *) pb);
+  memcpy(&a, pa, 16);
+  memcpy(&b, pb, 16);
   c = a % b;
-  *((v4si *) pc) = c;
-  //memcpy(pc, &c, 16);
-  return;
+  memcpy(pc, &c, 16);
 }
 
 void xor (unsigned char * pa, unsigned char * pb, unsigned char * pc) {
   v4si a, b, c;
-  a = *((v4si *) pa);
-  b = *((v4si *) pb);
+  memcpy(&a, pa, 16);
+  memcpy(&b, pb, 16);
   c = a ^ b;
-  *((v4si *) pc) = c;
-  //memcpy(pc, &c, 16);
-  return;
+  memcpy(pc, &c, 16);
 }
 
 void bitOr (unsigned char * pa, unsigned char * pb, unsigned char * pc) {
   v4si a, b, c;
-  a = *((v4si *) pa);
-  b = *((v4si *) pb);
+  memcpy(&a, pa, 16);
+  memcpy(&b, pb, 16);
   c = a | b;
-  *((v4si *) pc) = c;
-  //memcpy(pc, &c, 16);
-  return;
+  memcpy(pc, &c, 16);
 }
